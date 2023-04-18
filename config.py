ac45e89895c2abb069561812e6ca4c1c2f1f2a18
Xcod3bughunt3r <aliffusobar33@gmail.com>
2023-04-18 16:33:06 +0700

    wpbfpro

---
+++ config.py
@@ -0,0 +1,16 @@
+
+wordlist = "wordlist.txt"
+plugins_list = "plugins.txt"
+script_path = "wp-login.php"
+url = "http://localhost/wordpress/wp-login.php"
+threads = 5
+proxy = None
+
+# users
+username = None
+eu_gap_tolerance = 3
+
+# keywords
+min_keyword_len = 3
+min_frequency = 3
+ignore_with = ['&', ';', '<', '>', '(', ')', '}', '{', '=', '#', ':', '.', '_', '/', '\\']
